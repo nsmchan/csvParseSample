0cdbb7a078989b9d4013bdceb10ab7235acdadc4
minchan <minchan@DESKTOP-BKRTIQS>
2021-03-30 22:08:19 +0900

    first commit

---
+++ csvParseSample1/src/main/java/com/naver/ehfhfh1313/csvParseSample1/CSVParseSampleCore.java
@@ -0,0 +1,19 @@
+package com.naver.ehfhfh1313.csvParseSample1;
+
+import java.util.stream.Collectors;
+
+public class CSVParseSampleCore {
+	
+	
+
+	
+	public static void main(String[] args) throws Exception
+	{
+		EQDataLoader loader = new EQDataLoader();
+		loader.storeData(CSVParseSampleCore.class.getResourceAsStream("/sample.csv"));
+		String result = loader.getData().stream().map(d -> d.toString()).collect(Collectors.joining("\n"));
+		System.out.println(result);
+	}
+
+
+}
